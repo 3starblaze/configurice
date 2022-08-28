6c597304271c185049c55cd8eb6a986a98da5114
3starblaze <3starblaze@protonmail.com>
2022-08-28 12:43:59 +0300

    Make and use map->ini

--- src/three_starblaze/confi_raisu/core.clj
+++ src/three_starblaze/confi_raisu/core.clj
@@ -1,4 +1,15 @@
-(ns three-starblaze.confi-raisu.core)
+(ns three-starblaze.confi-raisu.core
+  (:import
+   [org.ini4j Ini]))
+
+(defn map->ini [m]
+  (let [ini (Ini.)]
+    (doseq [[section vars] m]
+      (doseq [[k v] vars]
+        (.put ini section k v)))
+    (with-out-str (.store ini *out*))))
 
 (defn main! []
-  (println "Hello world!"))
+  (println
+   (map->ini {"section foo" {"a" 1 "b" 2}
+              "section bar" {"c" 3 "d" 4}})))
