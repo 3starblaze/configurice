6e70dd93a1db88a193f7a904f5cde3b477cb6bcb
3starblaze <3starblaze@protonmail.com>
2023-06-26 11:32:51 +0300

    Add basic config schema

--- src/threestarblaze/configurice/core.clj
+++ src/threestarblaze/configurice/core.clj
@@ -1,11 +1,25 @@
 (ns threestarblaze.configurice.core
   (:require
    [clojure.java.io :as io]
+   [malli.core :as m]
+   [malli.error :as me]
    [threestarblaze.configurice.util :as util]))
+
+(def config-schema
+  [:map
+   [:foreign-configs
+    [:sequential [:map
+                  [:key :string]
+                  [:config [:map-of any? any?]]
+                  [:mapper fn?]
+                  [:command :string]]]]])
 
 (defn build-config!
   "Write foreign configuration and the intermediate file."
   [config]
+  (when-let [err (m/explain config-schema config)]
+    (throw (IllegalArgumentException. (str (me/humanize err)))))
+
   (doseq [foreign-config (:foreign-configs config)]
     (util/write-config! foreign-config))
   (let [edn-data (map (fn [foreign-config]
